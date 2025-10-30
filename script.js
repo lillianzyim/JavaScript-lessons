b32f7d5101ce0a8f0bddce5315c72f0ff4618564
lillianzyim <lillianzyim@gmail.com>
2025-10-30 09:20:27 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// create a function called hello with no parameters
+// function hello() {
+//     // window.alert("Hello loser")
+//     let firstname = prompt ("What is your name? ")
+//     window.alert("Hello " + firstname)
+// }
+
+// create a function that accepts parameters
+// function area(w,h) {
+//     window.alert(w * h)
+// }
+
+// function calculateArea(width,height) {
+//     let area = width * height;
+//     return area;
+// }
+
+// let wallOne=calculateArea(3,5);
+// window.alert(wallOne)
+
+// create object called pres
+// var pres = {
+//     // key-values
+//     fName: "Andrew",
+//     lName: "Yang",
+//     age: 45
+// };
+// object.key --> returns value
+// window.alert(pres.fName);
+// if the name of the property is a number, allowed but should avoid
+// if variable name is being used in place of a property
+// pres['fName'];
+
+
+// two objects with same values, but different names
+// var goodkid = {
+//     first: "kade",
+//     last: "kaita"
+// }
+// var badkid = {
+//     first: "quentin",
+//     last: "lau"
+// }
+
+// same properties with same property name, but different
+// var supremepizza = {
+//     toppings: ["pepperoni","olives","tomato"],
+//     price: 18.99,
+//     cheese: "provolone"
+// }
+// var pineapplepizza = {
+//     toppings: ["pineapple"],
+//     price: 8.99,
+//     cheese: "pepper jack"
+// }
+
+
+// our car constructor
+// function Car(color, brand) {
+//     this.color = color;
+//     this.brand = brand;
+// }
+// now we can make new cars easily!
+// const car1 = new Car('red','Toyota');
+// const car2 = new Car('blue','Honda');
+
+
+// object constructor
+// function lateasskids(name,excuses) {
+//     this.name = name;
+//     this.excuses = excuses;
+// }
+// let kid1 = new lateasskids('caleb','the fucking bus');
+// let kid2 = new lateasskids('leila','slow ass legs');
+// let kid3 = new lateasskids('vinson','weak bladder');
+// console.log(kid2.name + " excuse is " + kid2.excuses)
+
